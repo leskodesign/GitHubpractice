8a197a47df3a067e587eaa9d2ca774766bc72313
your name <you@example.com>
2016-03-02 10:44:34 -0500

    GitHubpractice

---
+++ GitHubpractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
